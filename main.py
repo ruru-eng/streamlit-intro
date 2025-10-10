f7c8fb84d17062f430b23db23c1b28db80ba53d2
Elliot <alexis.idzakovic@gmail.com>
2025-10-10 11:49:58 +0000

    Initial variable setup.

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+from player import Player
+
+run_details = pd.read_csv("run_details.csv")
+
+dungeons = run_details[run_details["player"] == "Ruana"].groupby(["dungeon_name"])["dungeon_name"].count().reset_index(name="record_count")
 
 
-st.write("Hello world")
-is_clicked = st.button("Click Me")
+print(dungeons.head(8))
+st.write("")
+st.bar_chart(x="dungeon_name",y="record_count",data=dungeons,x_label="",y_label="Count",color="#AAD372",sort="-record_count",horizontal=True)
+
+
+
---
+++ player.py
@@ -0,0 +1,5 @@
+import csv
+
+class Player:
+    def __init__(self):
+        pass
