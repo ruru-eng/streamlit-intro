65df070625982e7ff56a714986690b0676a56514
Elliot <alexis.idzakovic@gmail.com>
2025-10-14 15:14:51 +0000

    Added player selection, date range and reset button.

--- main.py
+++ main.py
@@ -1,15 +1,71 @@
 import streamlit as st
 import pandas as pd
 from player import Player
+from datetime import timedelta, date, datetime
+import altair as alt
 
-run_details = pd.read_csv("run_details.csv")
-
-dungeons = run_details[run_details["player"] == "Ruana"].groupby(["dungeon_name"])["dungeon_name"].count().reset_index(name="record_count")
-
-
-print(dungeons.head(8))
-st.write("")
-st.bar_chart(x="dungeon_name",y="record_count",data=dungeons,x_label="",y_label="Count",color="#AAD372",sort="-record_count",horizontal=True)
+# Load data
+df = pd.read_csv("run_details.csv")
+chars = pd.read_csv("characters.csv")
 
 
+# Set default dates
+start_date = date(2025, 8, 13)
+end_date = date.today()
+default_date_range = (start_date, end_date)
+
+# If not, then initialize it
+if "selected_date" not in st.session_state:
+    st.session_state.selected_date = default_date_range
+
+# Function to reset the date range
+def reset_date_range():
+    st.session_state.selected_date = default_date_range
+
+# Layout
+col1, col2 = st.columns(2)
+with st.container(border=True):
+    with col1:
+        player_selection = st.selectbox(
+            "Pick a player:",
+            [name.capitalize() for name in chars["name"].to_list()]
+        )
+        st.write("**Selected player:**", player_selection)
+        player = Player(player_selection, "") #name, realm
+
+    with col2:
+        # Reset button
+        if st.button("Reset Date Range"):
+            reset_date_range()
+        # Date range slider
+        selected_date = st.slider(
+            "Select a date range:",
+            min_value=start_date,
+            max_value=end_date,
+            value=st.session_state.selected_date,
+            format="MM/DD/YYYY",
+            step=timedelta(days=1),
+            key="selected_date"
+        )
+    # Bar chart
+    st.write("Dungeon distribution")
+    st.bar_chart(
+        data=player.fetch_dungeon_data_with_time(df,selected_date),
+        x="dungeon_name",
+        y="record_count",
+        x_label="Record Count",
+        y_label="",
+        sort="-record_count",
+        horizontal=True
+    )
+
+print(alt.theme.names())
+with st.container(border=True):
+
+    bar_chart = alt.Chart(player.fetch_level_data_with_time(df,selected_date)).mark_bar().encode(
+        x="mythic_level:N",
+        y="record_count:Q",
+        color="timed"
+    )
+    st.altair_chart(bar_chart, use_container_width=True)
 
--- player.py
+++ player.py
@@ -1,5 +1,37 @@
-import csv
+import pandas as pd
+import numpy as np
 
 class Player:
-    def __init__(self):
-        pass
+    all = []
+
+    # initialize player attributes
+    def __init__(self,name,realm):
+        self.__name = name
+        self.__realm = realm
+
+        Player.all.append(self)
+
+    @property 
+    def name(self):
+        return self.__name
+    @property 
+    def realm(self):
+        return self.__realm
+    
+    # filter date range
+    @staticmethod
+    def prepare_set(df,sel_date):
+        df["timed"] = np.where(df["num_chests"] == 0,"Untimed","Timed")
+        df["completed_at"] = pd.to_datetime(df["completed_at"]).dt.date
+        return df.loc[(df["completed_at"] >= sel_date[0]) & (df["completed_at"] <= sel_date[1])]
+            
+    # retrieve dungeon data in time range
+    def fetch_dungeon_data_with_time(self,df,sel_date):
+        f_df = Player.prepare_set(df,sel_date)
+        return f_df[f_df["player"] == self.__name].groupby(["dungeon_name"])["dungeon_name"].count().reset_index(name="record_count")
+    
+    # retrieve level data in time range
+    def fetch_level_data_with_time(self,df,sel_date):
+        f_df = Player.prepare_set(df,sel_date)
+        return f_df[f_df["player"] == self.__name].groupby(["mythic_level","timed"])["mythic_level"].count().reset_index(name="record_count")
+    
